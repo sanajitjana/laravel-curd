c6723875a6c3e5eb8bbec58ab56bdbb4e1e31391
Sanajt Jana <sanajitjana1402@gmail.com>
2021-06-05 15:41:20 +0530

    update landing url

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\contactController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('new-data');
 });
 
 Route::view('/new', 'new-data');
